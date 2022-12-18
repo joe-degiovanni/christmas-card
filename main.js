8590378e999056ececb5a75727911deb5bd9ba18
jdegiovanni <jdegiovanni@workfusion.com>
2022-12-18 14:45:23 -0600

    load gallery images dynamically

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function calculateSleepsTilXmas() {
     const today = new Date();
     const thisYear = today.getFullYear();
-    const nextChrismasYear = (today.getMonth() !== 11 || today.getDate() <= 25) ? thisYear :  thisYear + 1;
-    const timeDiff = new Date("12/25/"+nextChrismasYear).getTime() - today.getTime();
-    return Math.ceil(timeDiff / (1000*3600*24));
+    const nextChrismasYear = (today.getMonth() !== 11 || today.getDate() <= 25) ? thisYear : thisYear + 1;
+    const timeDiff = new Date("12/25/" + nextChrismasYear).getTime() - today.getTime();
+    return Math.ceil(timeDiff / (1000 * 3600 * 24));
 }
 
 function updateSleepsTilXmas(sleepsUntilXmas) {
@@ -20,11 +20,11 @@ function updateSleepsTilXmas(sleepsUntilXmas) {
         countdown.append(ele)
         countdown.append(document.createTextNode(" "))
     })
-    
+
     let text = document.createElement("div");
     text.innerText = "Sleeps until Christmas";
     text.className = "countdown-text"
-    
+
     countdown.append(text);
 }
 
@@ -33,18 +33,21 @@ function refreshSleepsTilXmas() {
     updateSleepsTilXmas(sleepsTilXmas)
 }
 
-function makeImgsClickable() {
-    const imgs = document.getElementsByTagName("img");
-    for (let i = 0; i < imgs.length; i++) {
-        imgs[i].onclick = (e) => openInNewTab(e.target.src)
+function loadGalleries() {
+    for (const gallery of document.getElementsByClassName("gallery")) {
+        for (let i = 1; i <= gallery.attributes["x-gallery-size"].value; i++) {
+            gallery.append(createImg(gallery, i));
+        }
     }
 }
 
-function openInNewTab(url) {
-    window.open(url, "_blank")
+function createImg(gallery, i) {
+    let img = document.createElement("img");
+    img.src = `img/${gallery.attributes["x-gallery-id"].value}/${i}.jpg`;
+    img.onclick = (e) => window.open(e.target.src, "_blank")
+    return img;
 }
 
-
-makeImgsClickable();
+loadGalleries();
 
 refreshSleepsTilXmas()
